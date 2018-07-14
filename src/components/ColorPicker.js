2a8e13a76afe16a699dc5ed6130d6a9a3872dffe
stuniel <stuniel@gmail.com>
2018-07-14 20:32:31 +0200

    Make ColorPicker an uncontrolled component

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,35 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import isEqual from 'lodash.isequal'
 
-import { ColorContext } from './withColor'
+import { ColorContext } from './ColorContext'
 import { createColorObject } from '../utils/color'
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  color: PropTypes.string,
+  defaultColor: PropTypes.string,
   onChange: PropTypes.func
 }
 
 const defaultProps = {
-  color: '#FFFFFF',
+  defaultColor: '#FFFFFF',
   onChange: () => {}
 }
 
-function ColorPicker(props) {
-  const {
-    children, color, onChange
-  } = props
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      color: createColorObject(this.props.defaultColor)
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-  return (
-    <ColorContext.Provider
-      value={{
-        color: createColorObject(color),
-        onChange
-      }}
-    >
-      {children}
-    </ColorContext.Provider>
-  )
+  componentDidUpdate(prevProps) {
+    if (prevProps.defaultColor !== this.props.defaultColor) {
+      const color = createColorObject(this.props.defaultColor)
+      this.setState({ color }) // eslint-disable-line react/no-did-update-set-state
+    }
+  }
+
+  handleChange(color) {
+    const { onChange } = this.props
+
+    this.setState({ color })
+
+    if (onChange) {
+      onChange(color)
+    }
+  }
+
+  render() {
+    const { children } = this.props
+
+    return (
+      <ColorContext.Provider
+        value={{
+          color: this.state.color,
+          onChange: this.handleChange
+        }}
+      >
+        {children}
+      </ColorContext.Provider>
+    )
+  }
 }
 
 ColorPicker.propTypes = propTypes
